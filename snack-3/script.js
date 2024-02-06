2f9963f56ea9a03f5e9d73ad102123ef3ab9a78b
post-coder <gabrielspanu.dev@gmail.com>
2024-02-06 10:38:49 +0100

    snack 3 optimized

--- snack-3/script.js
+++ snack-3/script.js
@@ -14,29 +14,21 @@ const array1 = [3,5,1];
 // inizializzo il secondo array
 const array2 = [1,2,3,4,5,6,7,8,9];
 
-// controllo quale sia il più piccolo
-if(array1.length < array2.length) {
+// controllo se il primo è più corto del secondo
+// ricordiamoci che il while può non essere eseguito neanche una volta
+// se la condizione non si verifica neanche una volta
+while (array1.length < array2.length) {
+  
+    // aggiungi elementi
+    array1.push(Math.floor(Math.random() * 100)+ 1)
 
-    // sinchè la lunghezza del primo array è inferiore di quella del secondo
-    do {
-        
-        // aggiungi elementi
-        // in questo caso sto aggiungendo gli elementi mancanti dal secondo array
-        // li prelevo comodamente usando come indice la lunghezza del primo visto che aumenta di uno in uno, che mi fa di volta in volta
-        // prelevare l'elemento successivo dal secondo
-        // array1.push(array2[array1.length]);
-        array1.push(Math.floor(Math.random() * 100)+ 1)
+}
 
-    } while (array1.length < array2.length) 
-
-} else {
+while(array1.length > array2.length) {
     // stessa cosa ma inversa
 
-    while (array2.length < array1.length) {
-        
-        array2.push(array1[array2.length]);
+    array2.push(array1[array2.length]);
 
-    }
 }
 
 console.log("Array 1:", array1);
